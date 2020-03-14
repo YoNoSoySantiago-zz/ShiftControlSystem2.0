bcdedf326c600faa4d5fee4d67dacc9e2e102492
Santiago Hurtado Solis <guapicito723@gmail.com>
2020-03-14 14:25:14 -0500

    archivos

---
+++ src/model/Auxd.java
@@ -0,0 +1,25 @@
+/*package model;
+import java.io.*;
+
+public class Auxd {
+	public static void main(String[]args) throws IOException {
+		
+		File file = new File("data/UserN.txt");
+		File fileR = new File("data/UserName.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		PrintWriter pw = new PrintWriter(fileR);
+		String str = br.readLine();
+		pw.print(str);
+		while(str !=null) {
+			str = br.readLine();
+			if(str!=null) {
+				pw.print(";"+str);
+			}	
+		}
+		pw.close();
+		br.close();
+		
+		
+	}
+}
+*/
